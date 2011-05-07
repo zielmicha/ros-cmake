f0b103fc5934d930239434427433686ce7a69565
Daniel Stonier <d.stonier@gmail.com>
2011-05-07 23:49:16 +0900

    still a hardcoded hack, but one that hacks for mingw_cross as well

--- 3rdparty/log4cxx-config.cmake
+++ 3rdparty/log4cxx-config.cmake
@@ -1,3 +1,10 @@
 find_library(LOG4CXX_LIBRRIES log4cxx)
-set(LOG4CXX_INCLUDE_DIRS /usr/include)
+
+if(MINGW)
+  set(LOG4CXX_INCLUDE_DIRS /opt/mingw/usr/i686-pc-mingw32/include)
+  set(LOG4CXX_LIBRARIES log4cxx aprutil-1 expat iconv apr-1 rpcrt4 shell32 ws2_32 advapi32 kernel32 msvcrt)
+else()
+  set(LOG4CXX_INCLUDE_DIRS /usr/include)
+endif()
+
 set(LOG4CXX_FOUND TRUE)
