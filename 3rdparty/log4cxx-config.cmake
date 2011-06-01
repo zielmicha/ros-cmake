47f53737e8e83cfdd083e6aa25cab85cac5366f8
Daniel Stonier <d.stonier@gmail.com>
2011-06-01 03:08:31 -0700

    Bugfix the log4cxx config, elseif not elif.

--- 3rdparty/log4cxx-config.cmake
+++ 3rdparty/log4cxx-config.cmake
@@ -1,11 +1,14 @@
-find_library(LOG4CXX_LIBRARIES log4cxx)
+#find_library(LOG4CXX_LIBRARIES log4cxx)
+
+message(STATUS "Looking for log4cxx...................")
 
 if(MINGW)
   set(LOG4CXX_INCLUDE_DIRS /opt/mingw/usr/i686-pc-mingw32/include)
   set(LOG4CXX_LIBRARIES log4cxx aprutil-1 expat iconv apr-1 rpcrt4 shell32 ws2_32 advapi32 kernel32 msvcrt)
-elif(MSVC)
+elseif(MSVC)
   set(LOG4CXX_INCLUDE_DIRS C:/opt/3rdparty/include)
   set(LOG4CXX_LIBRARIES C:/opt/3rdparty/log4cxx.lib)
+  message(STATUS "LOG4CXX LIBRARIES..........${LOG4CXX_LIBRARIES}")
 else()
   set(LOG4CXX_INCLUDE_DIRS /usr/include)
 endif()
